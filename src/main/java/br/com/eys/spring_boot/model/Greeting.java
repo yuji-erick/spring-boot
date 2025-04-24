55e7d42bea715c9725156d3c78e2c87f667debfb
Erick <yuji.erick@gmail.com>
2025-04-23 21:28:40 -0300

    Initial commit

---
+++ src/main/java/br/com/eys/spring_boot/model/Greeting.java
@@ -0,0 +1,4 @@
+package br.com.eys.spring_boot.model;
+
+public record Greeting(long id, String content) {
+}
---
+++ src/test/java/br/com/eys/spring_boot/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.eys.spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
